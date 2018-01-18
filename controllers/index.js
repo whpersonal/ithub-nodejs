39433cb5e657b60acabc47a51f92f69d5c72699e
lipengzhou <lpzmail@163.com>
2018-01-18 17:43:18 +0800

    添加数据库操作

--- controllers/index.js
+++ controllers/index.js
@@ -1,3 +1,13 @@
+const db = require('./db')
+
 exports.showIndex = (req, res) => {
-  res.render('index.html')
+  db.query('SELECT * FROM `topic_categories`', (err, results) => {
+    if (err) {
+      return res.send('500 Server Internal Error')
+    }
+    console.log(results)
+    res.render('index.html', {
+      topicCategories: results
+    })
+  })
 }
