6c69803057dba0760c84baddd3dacf3035cf9f0c
lipengzhou <lpzmail@163.com>
2018-01-18 16:32:46 +0800

    :tada: Initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+
+const app = express()
+
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('hello world')
+})
+
+app.listen(port, () => {
+  console.log(`Server is running at port ${port}`)
+  console.log(`  Please visit http://localhost:${port}/`)
+})
